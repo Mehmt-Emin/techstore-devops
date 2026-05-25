5c1f6142da53b33b0b76fe23ab9df828986210ad
Mehmt-Emin <mhmt0emn@gmail.com>
2026-05-25 21:36:24 +0300

    Update test_ui.py

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -23,10 +23,13 @@ BASE_URL = 'http://localhost:5000'
 def driver():
     """Headless Chrome sürücüsü."""
     options = Options()
-    options.add_argument('--headless')
+    options.add_argument('--headless=new')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
     options.add_argument('--window-size=1280,900')
+    options.add_argument('--remote-debugging-port=9222')
+    options.binary_location = "/usr/bin/google-chrome"
 
     try:
         from webdriver_manager.chrome import ChromeDriverManager
